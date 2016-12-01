d42a4ea72ccd555f37f16e6d6688e498867444b5
Sam Howat <sam@avelient.com>
2016-11-30 21:36:36 -0800

    Setup of basic raw html file

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Home</title>
+    <link rel="stylesheet" href="/dist/css/baseweb.min.css">
+  </head>
+
+  <body>
+
+    <!-- JS includes -->
+    <script src="//code.jquery.com/jquery-3.1.1.min.js"></script>
+    <script>!window.jQuery && document.write(unescape('%3Cscript src="/dist/js/libs/jquery.min.js"%3E%3C/script%3E'))</script>
+    <script src="/assets/js/scripts.min.js"></script>
+
+  </body>
+</html>
